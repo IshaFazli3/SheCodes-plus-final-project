7deefdef45c98074f9072826c5d428d6e5f7e9e3
Isha F <137927479+IshaFazli3@users.noreply.github.com>
2023-08-18 21:57:03 -0500

    Made corrections

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 function formatDate(timestamp) {
 let date = new Date(timestamp);
 let hours = date.getHours();
-  hours = `0${hours}`;
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+   
 let minutes = date.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
@@ -10,13 +13,12 @@ if (minutes < 10) {
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 let day = days[date.getDay()];
-return `${day} ${hours}:${minutes}`
+return `${day} ${hours}:${minutes}`;
 }
 
 
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = doucment.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -25,7 +27,7 @@ function displayTemperature(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-  let celsiusTemperature = response.data.main.temp;
+  celsiusTemperature = response.data.main.temp;
   
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
@@ -40,15 +42,13 @@ function displayTemperature(response) {
 
 function search(city){
   let apiKey = "c7bc0ta3bd1b1ab473o02ceeefd78e44";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${city}&key={apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
-
 }
-
 
 function handleSubmit(event) {
   event.preventDefault();
-  let cityInput = document.querySelector("city-input");
+  let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
 
