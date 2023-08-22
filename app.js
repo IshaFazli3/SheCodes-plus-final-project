0facae99cf55e9bb6eb87395b00da0fcce078ffb
Isha F <137927479+IshaFazli3@users.noreply.github.com>
2023-08-21 23:02:52 -0500

    Made HTML in JS

--- app.js
+++ app.js
@@ -23,6 +23,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML = forecastHTML + `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img src="https://assets.msn.com/weathermapdata/1/static/weather/Icons/taskbar_v10/Condition_Card/ClearNightV3.svg" 
+      alt="moon pic" width="42px">
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">18°</span>
+      <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+      </div>`;
+  });
+ 
+forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+    
+}
+
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -31,6 +54,8 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+
+
 
   celsiusTemperature = response.data.temperature.current;
 
@@ -75,6 +100,7 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -85,3 +111,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Seattle");
+displayForecast();
