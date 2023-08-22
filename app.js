2b20a6ffcc7c55e44ce24eb69b4ab23c5d234318
Isha F <137927479+IshaFazli3@users.noreply.github.com>
2023-08-22 00:59:51 -0500

    Displaying days and numbers accurately

--- app.js
+++ app.js
@@ -22,22 +22,34 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+
+return days[day];
+}
 
 function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
+  
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
     forecastHTML = forecastHTML + `
     <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-      <img src="https://assets.msn.com/weathermapdata/1/static/weather/Icons/taskbar_v10/Condition_Card/ClearNightV3.svg" 
+      <div class="weather-forecast-date">${forecastDay(forecastDay.dt)}</div>
+      
+      <img src="https://assets.msn.com/weathermapdata/1/static/weather/Icons/taskbar_v10/Condition_Card/C${forecastDay.weather[0].icons}.svg" 
       alt="moon pic" width="42px">
       <div class="weather-forecast-temperature">
-        <span class="weather-forecast-temperature-max">18°</span>
-      <span class="weather-forecast-temperature-min">12°</span>
+        <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+      <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
       </div>
       </div>`;
+    }
   });
  
 forecastHTML = forecastHTML + `</div>`;
